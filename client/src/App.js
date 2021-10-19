e8188de21470ffe3d763129794ea22b80db63778
cherryvue <yetiasg@gmail.com>
2021-10-19 16:46:36 +0200

    Working skaffold env

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Fib calculator with k8sss</h1>
+          <h1>Fib calculator with k8ss</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
